3209831a16800f3e9d75d0434843bbf6907b0b48
Olga Zhukova <zhukova.o.m@yandex.ru>
2021-01-16 16:43:05 +0500

    Implemented crossing behaviour

--- client/src/Cell/Cell.js
+++ client/src/Cell/Cell.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import styles from './Cell.module.css';
 
+const CellState = {
+    Empty: 0,
+    HalfCrossed: 1,
+    FullyCrossed: 2,
+};
+
+function getCrossStyle(cellState) {
+    switch (cellState) {
+        case CellState.HalfCrossed:
+            return styles.halfCrossed;
+        case CellState.FullyCrossed:
+            return styles.fullyCrossed;
+        default:
+            return '';
+    }
+}
+
 export const Cell = ({ cell }) => {
+    const [cellState, setCellState] = useState(CellState.Empty);
+
     if (!cell.text) {
         return <td className={cx(styles.cell, styles.empty)} />;
     }
@@ -12,7 +31,10 @@ export const Cell = ({ cell }) => {
     return (
         <td className={styles.cell}>
             <button
-                className={cx(styles.button, { [styles.pressable]: cell.text })}
+                className={cx(styles.button, getCrossStyle(cellState), {
+                    [styles.pressable]: cell.text,
+                })}
+                onClick={() => setCellState((cellState + 1) % 3)}
                 style={{
                     backgroundColor: cell.color,
                 }}
